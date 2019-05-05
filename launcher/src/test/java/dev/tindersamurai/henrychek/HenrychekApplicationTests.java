9f58d3909c2359c0044a60e2c718dbd278b2762a
HenryCo <henrycodev@gmail.com>
2019-05-05 19:57:10 +0200

    Initial commit

---
+++ launcher/src/test/java/dev/tindersamurai/henrychek/HenrychekApplicationTests.java
@@ -0,0 +1,16 @@
+package dev.tindersamurai.henrychek;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HenrychekApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ repository/src/main/java/dev/tindersamurai/henrychek/HenrychekApplication.java
@@ -0,0 +1,13 @@
+package dev.tindersamurai.henrychek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HenrychekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HenrychekApplication.class, args);
+	}
+
+}
